3d083621e04fe2ef50619980da03228daa6f1155
Manan <manan511shah@gmail.com>
2023-08-27 09:38:16 +0530

    added config file

--- housing/entity/config_entity.py
+++ housing/entity/config_entity.py
@@ -1,16 +1,23 @@
 from collections import namedtuple
 
-DataIngetionConfig = namedtuple("DataIngetionConfig",
-["dataset_download_url","tgz_download_url","raw_data_dir","ingested_train_dir","ingested_test_dir"])
 
-DataValidationConfig = namedtuple("DataValidationConfig",["schmea_file_path"])
+DataIngestionConfig=namedtuple("DataIngestionConfig",
+["dataset_download_url","tgz_download_dir","raw_data_dir","ingested_train_dir","ingested_test_dir"])
 
-DataTranformationConfig = namedtuple("DataTranformationConfig" , ["add_bedroom_per_room","transfored_train-dir"
-"transformed_test_dir","preprocessed_object_file_path"])
 
-ModelTrainerConfig = namedtuple("ModelTrainerConfig",["trained_model_file_path","base_accuracy"])
+DataValidationConfig = namedtuple("DataValidationConfig", ["schema_file_path","report_file_path","report_page_file_path"])
 
-ModelEvalutionConfig = namedtuple("ModelEvulationConfig",["model_evulation_file_path","time_stamp"])
+DataTransformationConfig = namedtuple("DataTransformationConfig", ["add_bedroom_per_room",
+                                                                   "transformed_train_dir",
+                                                                   "transformed_test_dir",
+                                                                   "preprocessed_object_file_path"])
 
-ModelPusherConfig = namedtuple("ModelPusherConfig",["export_dir_path"]) 
 
+ModelTrainerConfig = namedtuple("ModelTrainerConfig", ["trained_model_file_path","base_accuracy","model_config_file_path"])
+
+ModelEvaluationConfig = namedtuple("ModelEvaluationConfig", ["model_evaluation_file_path","time_stamp"])
+
+
+ModelPusherConfig = namedtuple("ModelPusherConfig", ["export_dir_path"])
+
+TrainingPipelineConfig = namedtuple("TrainingPipelineConfig", ["artifact_dir"])
